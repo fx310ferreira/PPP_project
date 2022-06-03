2a0e654f3d4bfe64e77293e027d06f1fc839b6b5
Frederico Ferreira <fx31003@gmail.com>
2022-06-03 18:38:19 +0100

    yes

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/tomas/OneDrive/Documentos/GitHub/PPP
+# Install script for directory: C:/Users/fx310/Documents/GitHub/PPP
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/tomas/OneDrive/Documentos/GitHub/PPP/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/fx310/Documents/GitHub/PPP/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
